4d9f912d0c3ec6718688933ce661ff88f8e2cbed
Sid Pagariya <sid@reducto.ai>
2025-06-02 09:02:16 -0700

    Update Parse Playground with customization, colors, and lots of updates

---
+++ src/lib/colors.ts
@@ -0,0 +1,14 @@
+export const bboxTypeColors = {
+  Header: "orange",
+  Footer: "gray",
+  Title: "red",
+  "Section Header": "red",
+  "Page Number": "orange",
+  "List Item": "yellow",
+  Figure: "purple",
+  Table: "green",
+  "Key Value": "indigo",
+  Text: "amber",
+  Comment: "gray",
+  Discard: "gray",
+};
