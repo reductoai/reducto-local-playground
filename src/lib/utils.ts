447767720a1e0dbc132bdf71680e9ee5cd21962a
Sid Pagariya <sid@reducto.ai>
2025-06-17 17:10:17 -0700

    feat: Add partial JSON parsing and local storage of configs to persist on loads

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getLocal = (key: string, fallback: string) => {
+  try {
+    const v = localStorage.getItem(key);
+    return v ?? fallback;
+  } catch {
+    return fallback;
+  }
+};
